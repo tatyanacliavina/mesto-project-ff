3bc64231014ccf5f6ae3dba6efc5115384e94506
Tatyana Klyavina <tatyana.cliavina@yandex.ru>
2024-02-13 23:02:51 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function addCard(element, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  
 
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = element.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+deleteButton.addEventListener("click", () => {deleteCard(cardElement)});
+
+  return cardElement;
+};
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+  cardElement.remove();
+};
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  placesList.append(addCard(item,deleteCard));
+});
